16a90094bf43fd68f12faf9f18a8758627d02f41
raihanabanu <raihanabanu.s@gmail.com>
2016-07-20 12:35:15 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,12 @@
+# multi
+import java.util.Scanner;
+public class Mul {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int m=s.nextInt();
+	for(int k=0;k<=16;k++){
+		int  j=m*k;
+		System.out.println(m+"*"+k+"="+j);
+		}
+		}
+}
